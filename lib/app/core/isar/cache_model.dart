be2192615bfd5e378e3457633e343f9b52c648fc
Amr Hossam <amrhossam8080@gmail.com>
2025-11-01 21:08:22 +0200

    Remove settings feature implementation including data models, repository, BLoC, and UI components. This includes the deletion of Isar source files, settings models, and associated event and state management, streamlining the application by eliminating unused settings functionality.

--- lib/app/core/isar/cache_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:isar/isar.dart';
-
-
-abstract base class CacheModel<D> {
-  CacheModel({this.id});
-  Id? id;
-
-  D toDomain();
-}
--- lib/falvours/statging/main_staging.dart
+++
@@ -1,6 +0,0 @@
-import 'package:engaz_task/app/app.dart';
-import 'package:engaz_task/app/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const MyApp());
-}
